0448c425e4179d934a776ce3ef50657e5be73dd4
Todd Baker <todd.baker90@gmail.com>
2021-07-14 15:06:37 -0400

    Re-arranging file structure of repo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace recipe_book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and Welcome to THE Recipe Book Console App!");
+            Console.WriteLine("");
+            Console.WriteLine("Please slect one of the following menu items:");
+            Console.WriteLine("1.) List all available recipes");
+            Console.WriteLine("2.) Search for a specific recipe name");
+            Console.WriteLine("3.) Conversion Tool");
+        }
+    }
+}
