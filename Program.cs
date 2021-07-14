080dce7cc668e34e6037eb29934e3f6bc2a4c0e6
Todd Baker <todd.baker90@gmail.com>
2021-07-14 16:07:33 -0400

    Built out basic user menu as a starting point

--- Program.cs
+++ Program.cs
@@ -6,12 +6,28 @@ namespace recipe_book
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello and Welcome to THE Recipe Book Console App!");
-            Console.WriteLine("");
-            Console.WriteLine("Please slect one of the following menu items:");
-            Console.WriteLine("1.) List all available recipes");
-            Console.WriteLine("2.) Search for a specific recipe name");
-            Console.WriteLine("3.) Conversion Tool");
+            bool isActive = true;
+
+            while(isActive)
+            { 
+                Console.WriteLine("Hello and Welcome to THE Recipe Book Console App!");
+                Console.WriteLine("");
+                Console.WriteLine("Please slect one of the following menu items:");
+                Console.WriteLine("1.) List all available recipes");
+                Console.WriteLine("2.) Search for a specific recipe name");
+                Console.WriteLine("3.) Conversion Tool");
+                Console.Write("Enter Selection: ");
+                string userInput = Console.ReadLine();
+
+                Console.WriteLine($"Your input: {userInput}");
+
+                if (userInput.ToLower() == "quit")
+                {
+                    Console.WriteLine("Closing App... Goodbye!");
+                    isActive = false;
+                }
+
+            }
         }
     }
 }
