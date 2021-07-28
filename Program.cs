57485747882afcb0030bc9ff92b5b139c2931162
Todd Baker <todd.baker90@gmail.com>
2021-07-27 20:40:01 -0400

    Implemented first iteration of the UnitConversionTool method and built it into the switch case for the menu. Will look to see how we can refactor it to make it more DRY.

--- Program.cs
+++ Program.cs
@@ -87,6 +87,29 @@ namespace recipe_book
                     }
                     return true;
                 case "3":
+                    bool thirdOption = true;
+                    while (thirdOption)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("----- Unit Conversion Tool -----");
+                        Console.WriteLine("Enter quantity of unit you want to convert (ex. 1/2 needs to input as 0.5): ");
+                        var quantityInput = Console.ReadLine();
+                        Console.WriteLine("Enter unit you are wanting to convert from (ex. cups, ounces, or grams): ");
+                        var initialUnitInput = Console.ReadLine();
+                        Console.WriteLine("Enter the desired unit you are wanting to convert to (ex. cups, ounces, or grams): ");
+                        var desiredUnitInput = Console.ReadLine();
+
+                        UnitConversionTool(Convert.ToDouble(quantityInput), initialUnitInput.ToLower(), desiredUnitInput.ToLower());
+
+                        Console.WriteLine("\nPress Enter to do another unit conversion.");
+                        Console.WriteLine("Enter 1 to go back to main menu!");
+                        Console.Write("User Entry: ");
+                        var backOut = Console.ReadLine();
+                        if (backOut == "1")
+                        {
+                            thirdOption = false;
+                        }
+                    }
                     return true;
                 case "4":
                     Console.WriteLine("Exiting App....Good Bye!");
@@ -113,10 +136,12 @@ namespace recipe_book
         {
             List<Recipe> recipes = new List<Recipe>(); 
             recipes = DeserializeRecipe();
+            int counter = 1;
 
             foreach (var recipe in recipes)
             {
-                Console.WriteLine(recipe.Name);
+                Console.WriteLine($"{counter}.) {recipe.Name}");
+                counter++;
             }
 
         }
@@ -126,13 +151,13 @@ namespace recipe_book
             List<Recipe> recipes = new List<Recipe>();
             recipes = DeserializeRecipe();
 
-            bool checkIfListContains = recipes.Any(p => p.Name == searchInput.ToLower()); // Boolean variable to hold if the searchInput exists within the list of Recipe objects
+            bool checkIfListContains = recipes.Any(p => p.Name.ToLower() == searchInput.ToLower()); // Boolean variable to hold if the searchInput exists within the list of Recipe objects
             
             if (checkIfListContains == true) //First check if the searchInput is contained within the recipes list and if not print out Invalid Search
             {
                 foreach (var recipe in recipes)
                 {
-                    if (searchInput.ToLower() == recipe.Name)
+                    if (searchInput.ToLower() == recipe.Name.ToLower())
                     {
                         Console.Clear();
                         Console.WriteLine("The recipe you have chosen:");
@@ -161,6 +186,52 @@ namespace recipe_book
                 Console.WriteLine($"{searchInput} does not exist in the Recipe Book, please try again!");
             }
             
+        }
+
+        public static void UnitConversionTool(double quantity, string initialUnit, string desiredUnit)
+        {
+            double convertedQuantity;
+
+            if(initialUnit == "cups" && desiredUnit == "grams")
+            {
+                double gramsPerCup = 236.5;
+                convertedQuantity = quantity * gramsPerCup;
+                Console.WriteLine($"\n{quantity} cup(s) is approximately {convertedQuantity} grams!");
+            }
+            else if (initialUnit == "grams" && desiredUnit == "cups")
+            {
+                double gramsPerCup = 236.5;
+                convertedQuantity = quantity / gramsPerCup;
+                Console.WriteLine($"\n{quantity} grams is approximately {convertedQuantity} cup(s)!");
+            }
+            else if (initialUnit == "grams" && desiredUnit == "ounces")
+            {
+                double ouncesPerGram = 0.03;
+                convertedQuantity = quantity * ouncesPerGram;
+                Console.WriteLine($"\n{quantity} grams is approximately {convertedQuantity} ounce(s)!");
+            }
+            else if (initialUnit == "ounces" && desiredUnit == "grams")
+            {
+                double ouncesPerGram = 0.03;
+                convertedQuantity = quantity / ouncesPerGram;
+                Console.WriteLine($"\n{quantity} ounce(s) is approximately {convertedQuantity} grams!");
+            }
+            else if (initialUnit == "cups" && desiredUnit == "ounces")
+            {
+                double ouncesPerCup = 8.34;
+                convertedQuantity = quantity * ouncesPerCup;
+                Console.WriteLine($"\n{quantity} cup(s) is approximately {convertedQuantity} ounces!");
+            }
+            else if (initialUnit == "ounces" && desiredUnit == "cups")
+            {
+                double ouncesPerCup = 8.34;
+                convertedQuantity = quantity / ouncesPerCup;
+                Console.WriteLine($"\n{quantity} ounce(s) is approximately {convertedQuantity} cups!");
+            }
+            else
+            {
+                Console.WriteLine("\nUnable to read units, retry and make sure to either user \"cups\", \"ounces\", or \"grams\".");
+            }
         }
     }
 }
